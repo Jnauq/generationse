cc0b067a1083ea3e23741317f426c8fd8845d681
Jnauq <35784417+Jnauq@users.noreply.github.com>
2019-08-30 11:14:00 -0700

    Create metaimg.py

---
+++ mri/metaimg.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#######################################################################################
+#  View metadata for a single Dicom file.
+
+#  Usage: python metaimg.py <full path of file>
+#  ie: python metaimg.py /home/justin/testRCH_feb/Data/cd21/SDY00003/SRS00002/IMG00000
+#
+#         python <full path of script>/metaimg.py IMG00000
+#  ie.  python /home/justin/PycharmProjects/MRI/metaimg.py IMG00000
+#######################################################################################
+
+import pydicom
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Usage: metaimg.py <input_image>")
+    sys.exit(1)
+
+rootdir = sys.argv[1]
+
+pydcm = pydicom.dcmread(rootdir)
+print(pydcm)
+
+
